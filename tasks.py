6a666a6bc0a5b7875b10131eba44e3fdfaef005f
MonicaBarna <monica@robocorp.com>
2023-07-11 22:45:30 +0300

    Address comments

--- tasks.py
+++ tasks.py
@@ -5,11 +5,9 @@ from RPA.PDF import PDF
 @task
 def robot_spare_bin_python():
     """Robot to enter weekly sales data into the RobotSpareBin Industries Intranet."""
-
     # Without this, nothing is visible as it is running headless
     browser.configure(
         slowmo=100,
-        browser_engine="chromium",
         headless=False,
     )
     open_the_intranet_website()
@@ -22,12 +20,10 @@ def robot_spare_bin_python():
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
-
     browser.goto("https://robotsparebinindustries.com/")
 
 def log_in():
     """Fills in the login form and clicks the 'Log in' button"""
-
     page = browser.page()
     page.fill("#username", "maria")
     page.fill("#password", "thoushallnotpass")
@@ -35,13 +31,10 @@ def log_in():
 
 def download_excel_file():
     """Downloads data from the given URL"""
-
-    # Set overwrite=True in case you want your file to be replaced on every download
     http.download(url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
 
 def fill_and_submit_sales_form(sales_rep):
     """Fills in the sales data and click the 'Submit' button"""
-
     page = browser.page()
 
     page.fill("#firstname", sales_rep["First Name"])
@@ -52,7 +45,6 @@ def fill_and_submit_sales_form(sales_rep):
 
 def fill_form_with_excel_data():
     """Read data from excel and pass it row by row to fill_and_submit_sales_form() function"""
-
     workbook = excel.open_workbook("SalesData.xlsx")
     worksheet = workbook.worksheet("data").as_table(header=True)
 
@@ -61,13 +53,11 @@ def fill_form_with_excel_data():
 
 def collect_results():
     """Take a screenshot of the page"""
-
     page = browser.page()
     page.screenshot(path="output/sales_summary.png")
 
 def export_as_pdf():
     """Export the data to a pdf file"""
-
     page = browser.page()
 
     sales_results = page.locator("#sales-results")
@@ -78,6 +68,5 @@ def export_as_pdf():
 
 def log_out():
     """Presses the 'Log out' button"""
-    
     page = browser.page()  
     page.click("text=Log out")
